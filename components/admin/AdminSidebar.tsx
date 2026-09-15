c30ffe53435a1607b17ceff5274507a1f6e807a8
Dicky Aldian Pratama <email.bgdicky123@gmail.com>
2026-09-15 07:13:59 +0700

    Fix incorrect import path for next-themes in AdminSidebar

--- components/admin/AdminSidebar.tsx
+++ components/admin/AdminSidebar.tsx
@@ -3,7 +3,7 @@
 import Link from "next/link";
 import Image from "next/image";
 import { usePathname } from "next/navigation";
-import { useTheme } from "next/themes";
+import { useTheme } from "next-themes";
 import { useEffect, useState } from "react";
 import { 
   LayoutDashboard, 
